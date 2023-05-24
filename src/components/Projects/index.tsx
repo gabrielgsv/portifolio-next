6452fa6e72c9def29aade8fa2f9d44ab81868886
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-05-24 15:52:00 -0300

    added url for icons

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -10,38 +10,45 @@ const Projects = () => {
       src: "/agriculture.png",
       alt: "Agriculture icon",
       name: "Agriculture",
+      url: "https://agriculture-gabrielsoaresvilela.vercel.app",
     },
     {
       src: "/ecommerce.png",
       alt: "Ecommerce icon",
       name: "Ecommerce",
+      url: "https://ecommerce-org.vercel.app/",
     },
     {
       src: "/planet.png",
       alt: "ThreeJs Planet icon",
       name: "ThreeJs Planet",
+      url: "https://threejs-earth.vercel.app/",
     },
     {
       src: "/pet.png",
       alt: "Adote um Pet icon",
       name: "Adote um Pet",
+      url: "https://adote-um-pet-web.vercel.app/",
     },
     {
       src: "/drink.png",
       alt: "Drinks  icon",
       name: "Drinks ",
+      url: "https://all-of-the-drinks.vercel.app/",
     },
     {
       src: "/marvel.png",
       alt: "Marvel Wiki icon",
       name: "Marvel Wiki",
+      url: "https://marvel-wiki.vercel.app/",
     },
   ];
   return (
     <>
       <div className={componentsStyle.container}>
-        <div>
+        <div style={{ textAlign: "center" }}>
           <ProjectsAnimation />
+          <p style={{ marginTop: "-40px", fontSize: 25 }}>Clique aqui ☝️</p>
         </div>
         <div className={style.container}>
           <ProjectIcon icons={icons} />
