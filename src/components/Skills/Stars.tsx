9a56ae87218b8d8511a2032d869fb34f8ebd478d
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-05-25 17:12:16 -0300

    fix skills ref

--- src/components/Skills/Stars.tsx
+++ src/components/Skills/Stars.tsx
@@ -2,17 +2,13 @@
 
 import { useEffect, useRef } from "react";
 import { useRive, useStateMachineInput } from "@rive-app/react-canvas";
-import useOnScreen from "../../services/useOnScreen";
 
 type Props = {
   number: number;
+  isVisible: boolean;
 };
 
-const Stars = ({ number }: Props) => {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const entry = useOnScreen(ref, {});
-  const isVisible = !!entry?.isIntersecting;
-
+const Stars = ({ number, isVisible }: Props) => {
   const { rive, RiveComponent } = useRive({
     src: "/rating.riv",
     stateMachines: "State Machine 1",
@@ -37,11 +33,7 @@ const Stars = ({ number }: Props) => {
     }
   }, [isVisible]);
 
-  return (
-    <div ref={ref} onClick={() => "clicou aqui"}>
-      <RiveComponent className="w-[300px] h-[75px] rounded-full" />
-    </div>
-  );
+  return <RiveComponent className="w-[300px] h-[75px] rounded-full" />;
 };
 
 export default Stars;
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,41 +1,49 @@
+"use client";
+
 import SuperManAnimation from "./SuperManAnimation";
 import Stars from "./Stars";
+import { useRef } from "react";
+import useOnScreen from "../../services/useOnScreen";
 
 const Skills = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const entry = useOnScreen(ref, {});
+  const isVisible = !!entry?.isIntersecting;
+
   return (
     <>
       <div className="flex flex-wrap items-center justify-center gap-20">
-        <div className="text-[20pt] w-fit">
+        <div className="text-[20pt] w-fit" ref={ref}>
           <div className="flex items-center justify-between gap-5">
-            ReactJs <Stars number={5} />
+            ReactJs <Stars number={5} isVisible={isVisible} />
           </div>
 
           <div className="flex items-center mt-4 justify-between gap-5">
-            NextJs <Stars number={4} />
+            NextJs <Stars number={4} isVisible={isVisible} />
           </div>
 
           <div className="flex items-center mt-4 justify-between gap-5">
-            Typescript <Stars number={3} />
+            Typescript <Stars number={3} isVisible={isVisible} />
           </div>
 
           <div className="flex items-center mt-4 justify-between gap-5">
-            Redux <Stars number={4} />
+            Redux <Stars number={4} isVisible={isVisible} />
           </div>
 
           <div className="flex items-center mt-4 justify-between gap-5">
-            React Native <Stars number={2} />
+            React Native <Stars number={2} isVisible={isVisible} />
           </div>
 
           <div className="flex items-center mt-4 justify-between gap-5">
-            NodeJs <Stars number={4} />
+            NodeJs <Stars number={4} isVisible={isVisible} />
           </div>
 
           <div className="flex items-center mt-4 justify-between gap-5">
-            Express <Stars number={3} />
+            Express <Stars number={3} isVisible={isVisible} />
           </div>
 
           <div className="flex items-center mt-4 justify-between gap-5">
-            NestJs <Stars number={3} />
+            NestJs <Stars number={3} isVisible={isVisible} />
           </div>
         </div>
 
