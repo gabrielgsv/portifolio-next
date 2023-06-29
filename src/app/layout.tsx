776d9850d943bf182532dfad5c1b5cfbde434f32
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-06-29 18:52:47 -0300

    added google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata = {
   title: "Gabriel | Desenvolvedor Web e Mobile",
   description:
     "Esse é o meu site pessoal, onde você pode saber mais sobre mim. Tenho experiência com desenvolvimento front-end utilizando React e suas tecnologias. Neste site, você pode ver alguns dos meus projetos e entrar em contato comigo.",
+  verification: {
+    google: "QLfjmJeLgGFF9RPbmOB9S3jQDSJYnr_YdDjH6XEknfo",
+  },
 };
 
 export default function RootLayout({
