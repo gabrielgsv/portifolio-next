cb522ea9b48f7507115022e378bbf86f2cadc5c5
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-06-29 18:27:03 -0300

    remake projects components

--- src/components/Projects/ProjectIcon/index.tsx
+++ src/components/Projects/ProjectIcon/index.tsx
@@ -1,38 +1,67 @@
-import React from "react";
 import Image from "next/image";
-import style from "./projectIcon.module.css";
 import Link from "next/link";
+import { ProjectIconType } from "..";
 
-export interface ProjectIconProps {
-  icons: {
-    src: string;
-    alt: string;
-    name: string;
-    url: string;
-  }[];
-}
+type ProjectIconProps = {
+  icons: ProjectIconType[];
+};
 
 const ProjectIcon = ({ icons }: ProjectIconProps) => {
   return (
     <>
       {icons.map((icon, index) => (
-        <Link
-          href={icon.url}
-          className="w-28 h-28 md:w-[150px] md:h-[150px] flex flex-col items-center justify-center cursor-pointer"
+        <div
           key={index}
-          target="_blank"
+          className="card w-[350px] pt-5 border-solid border-2 border-stone-400 shadow-xl"
         >
-          <div className="w-16 h-16 md:w-[100px] md:h-[100px] rounded-full bg-white flex justify-center items-center border-solid border-2 border-black">
-            <Image
-              src={icon.src}
-              alt={icon.alt}
-              width={80}
-              height={80}
-              className="rounded-full max-w-full max-h-full"
-            />
+          <div className="w-full flex justify-center">
+            <div className="bg-white w-[100px] h-[100px] rounded-full flex justify-center items-center border-solid border-2 border-stone-400">
+              <Image
+                src={icon.src}
+                alt={icon.alt}
+                width={80}
+                height={80}
+                className="rounded-full max-w-full max-h-full"
+              />
+            </div>
           </div>
-          <div className="mt-2">{icon.name}</div>
-        </Link>
+
+          <div className="card-body">
+            <h2 className="card-title">{icon.name}</h2>
+            <p>{icon.description}</p>
+            <div className="card-actions justify-around mt-5">
+              <Link href={icon.url} target="_blank">
+                <button className="btn bg-emerald-600 rounded-full">
+                  <Image
+                    src="/web.svg"
+                    alt="Link do projeto"
+                    width={20}
+                    height={20}
+                    className="mr-2"
+                  />
+                  <span className="capitalize text-base text-white">
+                    Projeto
+                  </span>
+                </button>
+              </Link>
+
+              <Link href={icon.github} target="_blank">
+                <button className="btn bg-stone-700 rounded-full">
+                  <Image
+                    src="/github.svg"
+                    alt="Link do projeto"
+                    width={20}
+                    height={20}
+                    className="mr-2"
+                  />
+                  <span className="capitalize text-base text-white">
+                    Github
+                  </span>
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       ))}
     </>
   );
